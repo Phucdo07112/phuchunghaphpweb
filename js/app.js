933ab2f47f02c42b8e60a9b7d23e0dda73b9b8c1
Phucdo07112 <phucdo0711@gmail.com>
2021-11-19 10:34:50 +0700

    up
    up

--- js/app.js
+++ js/app.js
@@ -10,6 +10,18 @@ window.addEventListener('scroll', function() {
     const imgDow = document.querySelector('.intro-dow');
     imgDow.classList.toggle('toggle', window.scrollY > 1000);
 })
+// product
+const imgItem = document.querySelectorAll('.product-img__item');
+
+imgItem.forEach(function(value,index) {
+    const homeImg = document.querySelector('.product-img');
+    value.onclick = function() {
+        homeImg.src = value.src
+    }
+})
+
+
+
 var swiper = new Swiper(".intro-img", {
         spaceBetween: 30,
         loop: true,
@@ -21,4 +33,25 @@ var swiper = new Swiper(".intro-img", {
           el: ".swiper-pagination",
           clickable: true,
         },
+      });
+
+var swiper = new Swiper(".intro-product", {
+        slidesPerView: 4,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        grid: {
+          rows: 2,
+        },
+        spaceBetween: 0,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        loop: true
       });
